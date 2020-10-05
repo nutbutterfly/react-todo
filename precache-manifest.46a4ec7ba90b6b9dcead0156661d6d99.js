b3860dccd31d3938100506e242f4bb5b727fba43
Natthapon Pinyo <natthapon.p@aware.co.th>
2020-10-05 11:59:15 +0700

    Updates

---
+++ precache-manifest.46a4ec7ba90b6b9dcead0156661d6d99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e37280f6a5390631f3fa56893141724",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "ecd82fb5d868c2500160",
+    "url": "/react-todo/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "db0a95f029a64f1581e8",
+    "url": "/react-todo/static/css/main.c7c992b6.chunk.css"
+  },
+  {
+    "revision": "ecd82fb5d868c2500160",
+    "url": "/react-todo/static/js/2.1f3aaafb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-todo/static/js/2.1f3aaafb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db0a95f029a64f1581e8",
+    "url": "/react-todo/static/js/main.dc132d25.chunk.js"
+  },
+  {
+    "revision": "b81e150b308975bc2389",
+    "url": "/react-todo/static/js/runtime-main.89ae8f88.js"
+  }
+]);
