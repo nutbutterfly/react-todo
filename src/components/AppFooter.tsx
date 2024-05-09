e37d85e6f7cb046399ef7e452afd3571467025fc
Natthapon Pinyo <natthapon.p@aware.co.th>
2024-05-09 09:14:46 +0700

    fix dependency vulnerability

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function AppFooter() {
     return (
-        <div className="my-2 text-muted text-center">&copy; All rights reserved <strong>NutButterflY</strong> 2020</div>
+        <div className="my-2 text-muted text-center">&copy; All rights reserved <strong>NutButterflY</strong> 2024</div>
     );
 }
 
